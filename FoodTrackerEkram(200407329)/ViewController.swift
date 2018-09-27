c6ef58d01c7a4044c112381521b1de0d9ec153c6
Administrator <shl154@udml7.local>
2018-09-27 12:38:50 -0600

    Initial Commit

---
+++ FoodTrackerEkram(200407329)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTrackerEkram(200407329)
+//
+//  Created by S Haque on 2018-09-27.
+//  Copyright © 2018 S Haque. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
